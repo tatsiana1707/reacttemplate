6b827eef60d90faa3bad6920faab485a13663578
tatsiana1707 <tannysha1987@mail.ru>
2022-09-09 11:32:16 +0300

    add react template

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Module from './module';
+import './index.scss';
+
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(<Module/>);
---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import spider from './img/spider.png';
+
+const Module = () => (
+    <>
+    <h1>Hello, React!</h1>
+    <img width={200} src={spider} />
+    </>
+);
+
+export default Module;
